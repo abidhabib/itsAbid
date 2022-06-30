1e6f1554c9a70394ec584237083f578f360e9fef
Abid Habib ✅ <41414028+abidhabib@users.noreply.github.com>
2022-07-01 03:25:45 +0500

    Update script.js

--- weatherApp/script.js
+++ weatherApp/script.js
@@ -17,17 +17,17 @@ const proxy="https://cors-anywhere.herokuapp.com/"
             
             lat=position.coords.latitude;
             long=position.coords.longitude;
-            const api=`${proxy}https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid={db758e26ce89f2110819cfbbf048a06d`
+            const api=`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid={db758e26ce89f2110819cfbbf048a06d`
         fetch(api).then((response)=>{
             return response.json();
         })})
 .then(data=>{
     const {name}=data;
-    const {feels_alike}=data.main;
+    const {feels_like}=data.main;
     const {id,main}=data.weather[0];
     loc.textContent=name;
     climate.textContent=main;
-    tempvalue.textContent=Math.round(feels_alike);
+    tempvalue.textContent=Math.round(feels_like);
 
 }) 
 }})
